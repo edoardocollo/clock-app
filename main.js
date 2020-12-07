1c70c46ea61187e5d83e82710c5d16419a54972c
edoardocollo <edoardo.collo@gmail.com>
2020-12-07 12:17:16 +0100

    bit of style

--- main.js
+++ main.js
@@ -1,44 +1,44 @@
-//
-// var documentTarget = $('#main_container');
-// var audio = document.getElementById("myAudio");
-// var displayOra = $('#display_ora');
-//
-// // VISUALIZZA ORA A SCHERMO
-// //////////////////////////////
-// setInterval(function () {
-//   var oraAttuale = new Date();
-//   var secondi = oraAttuale.getSeconds();
-//   var minuti = oraAttuale.getMinutes();
-//   var ore = oraAttuale.getMonth();
-//   displayOra.text(`${ore}:${minuti}:${secondi}`)
-//
-// }, 1000);
-//
-//
-//
-//
-// // FUNZIONE SVEGLIA
-// //////////////////////////
-//
-// function sveglia(setOre,setMinuti,setSecondi) {
-//
-//     var now = new Date();
-//     var secondi = now.getSeconds();
-//     var minuti = now.getMinutes();
-//     var ore = now.getMonth();
-//     var setSecondi;
-//     var setMinuti;
-//     var setOre;
-//
-//     // documentTarget.text(`${ore}:${minuti}:${secondi}`);
-//     if (secondi == setSecondi && minuti == setMinuti && ore == setOre) {
-//       audio.play();
-//     }
-// }
-//
-// setInterval(
-// sveglia
-// , 1000,11,36,10 );
+
+var documentTarget = $('#main_container');
+var audio = document.getElementById("myAudio");
+var displayOra = $('#display_ora');
+
+// VISUALIZZA ORA A SCHERMO
+//////////////////////////////
+setInterval(function () {
+  var oraAttuale = new Date();
+  var secondi = oraAttuale.getSeconds();
+  var minuti = oraAttuale.getMinutes();
+  var ore = oraAttuale.getMonth();
+  displayOra.text(`${ore}:${minuti}:${secondi}`)
+
+}, 1000);
+
+
+
+
+// FUNZIONE SVEGLIA
+//////////////////////////
+
+function sveglia(setOre,setMinuti,setSecondi) {
+
+    var now = new Date();
+    var secondi = now.getSeconds();
+    var minuti = now.getMinutes();
+    var ore = now.getMonth();
+    var setSecondi;
+    var setMinuti;
+    var setOre;
+
+    // documentTarget.text(`${ore}:${minuti}:${secondi}`);
+    if (secondi == setSecondi && minuti == setMinuti && ore == setOre) {
+      audio.play();
+    }
+}
+
+setInterval(
+sveglia
+, 1000,11,36,10 );
 
 // CRONOMETRO
 /////////////////////
