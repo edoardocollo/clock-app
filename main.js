a7468d9137b4ff21859c5bbbd6a15c9fff30b670
edoardocollo <edoardo.collo@gmail.com>
2020-12-07 11:37:17 +0100

    aggiunto orologio

--- main.js
+++ main.js
@@ -1,24 +1,39 @@
-// Set variable to current date and time
 
-// View the output
 var documentTarget = $('#main_container');
 var audio = document.getElementById("myAudio");
+var displayOra = $('#display_ora');
+
+// VISUALIZZA ORA A SCHERMO
+//////////////////////////////
+setInterval(function () {
+  var oraAttuale = new Date();
+  var secondi = oraAttuale.getSeconds();
+  var minuti = oraAttuale.getMinutes();
+  var ore = oraAttuale.getMonth();
+  displayOra.text(`${ore}:${minuti}:${secondi}`)
+
+}, 1000);
 
 
+
+
+// FUNZIONE SVEGLIA
+//////////////////////////
 
 function sveglia(setOre,setMinuti,setSecondi) {
-  var now = new Date();
-  var secondi = now.getSeconds();
-  var minuti = now.getMinutes();
-  var ore = now.getMonth();
-  var setSecondi;
-  var setMinuti;
-  var setOre;
 
-  // documentTarget.text(`${ore}:${minuti}:${secondi}`);
-  if (secondi == setSecondi && minuti == setMinuti && ore == setOre) {
-    audio.play();
-  }
+    var now = new Date();
+    var secondi = now.getSeconds();
+    var minuti = now.getMinutes();
+    var ore = now.getMonth();
+    var setSecondi;
+    var setMinuti;
+    var setOre;
+
+    // documentTarget.text(`${ore}:${minuti}:${secondi}`);
+    if (secondi == setSecondi && minuti == setMinuti && ore == setOre) {
+      audio.play();
+    }
 }
 
 
@@ -28,4 +43,4 @@ function sveglia(setOre,setMinuti,setSecondi) {
 
 setInterval(
 sveglia
-, 1000,11,20,50 );
+, 1000,11,36,10 );
